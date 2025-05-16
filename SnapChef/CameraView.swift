f70ce78c0f2563f0e82f242f18d9bfba798c0206
gasimmons <gasimmons@ucsd.edu>
2025-05-15 19:15:28 -0700

    Saves image to screen, not to camera roll

--- SnapChef/CameraView.swift
+++ SnapChef/CameraView.swift
@@ -32,6 +32,29 @@ struct CameraView: View {
                     }
                     .padding(.bottom, 40)
                 }
+                
+                // Need to run the cv model on the image
+                // Print a list of identified foods
+                // Also provide a text entry to provide more foods not scene or not detected
+                if let image = cameraManager.capturedImage {
+                    Color.black.opacity(0.6)
+                        .ignoresSafeArea()
+                    VStack {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .padding()
+
+                        Button("Dismiss") {
+                            cameraManager.capturedImage = nil
+                        }
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    }
+                    .transition(.opacity)
+                }
             } else {
                 Color.black
                 Text("Camera Access Needed")
@@ -42,5 +65,6 @@ struct CameraView: View {
         .onAppear {
             cameraManager.checkPermissionAndSetup()
         }
+        .animation(.easeInOut, value: cameraManager.capturedImage)
     }
 }
